6753b8922cf2f95507c3f154d87fff9e3d16ba92
BlueGameMC <101770837+BlueGameMC@users.noreply.github.com>
2024-03-01 14:43:47 -0800

    Add files via upload

---
+++ Acerola/car.gd
@@ -0,0 +1,37 @@
+extends VehicleBody3D
+
+var max_rpm = 500
+var max_tourqe = 200
+
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("Right","Left") * 0.4,2*delta)
+	var acceleration = Input.get_axis("Back","Forward")*1.5
+	var rpm = abs($BL.get_rpm())
+	$BL.engine_force = acceleration * max_tourqe * (1 - rpm / max_rpm)
+	rpm = abs($BR.get_rpm())
+	$BR.engine_force = acceleration * max_tourqe * (1 - rpm / max_rpm)
+	if Input.get_action_strength("Drift")>0.5:
+		#$BL.use_as_traction = false
+		#$BR.use_as_traction = false
+		steering = lerp(steering, Input.get_axis("Right","Left") * 0.4,4*delta)
+		$BR.wheel_friction_slip = lerp($BR.wheel_friction_slip, 1.5, 8*delta)
+		$BL.wheel_friction_slip = lerp($BL.wheel_friction_slip, 1.5, 8*delta)
+		$FR.wheel_friction_slip = lerp($FR.wheel_friction_slip, 1.5, 8*delta)
+		$FL.wheel_friction_slip = lerp($FL.wheel_friction_slip, 1.5, 8*delta)
+	else:
+		#$BL.use_as_traction = true
+		#$BR.use_as_traction = true
+		
+		$BR.wheel_friction_slip = lerp($BR.wheel_friction_slip, 10.5, 8*delta)
+		$BL.wheel_friction_slip = lerp($BL.wheel_friction_slip, 10.5, 8*delta)
+		$FR.wheel_friction_slip = lerp($FR.wheel_friction_slip, 10.5, 8*delta)
+		$FL.wheel_friction_slip = lerp($FL.wheel_friction_slip, 10.5, 8*delta)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var camera = $"../Camera3D"
+	
+	$"../Camera3D".position = lerp($"../Camera3D".position, $Body/CameraTarget.global_position, 2*delta)
+	
+	camera.look_at(global_position)
